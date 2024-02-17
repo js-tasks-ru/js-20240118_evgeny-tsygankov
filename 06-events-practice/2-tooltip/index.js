e382cb8b6e67dafd807332f99634173336012de2
Evgeny <evtsyga@gmail.com>
2024-02-17 20:52:42 +0300

    feat: add 06 module 2-tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,75 @@
 class Tooltip {
-  initialize () {
+  static #onlyInstance;
 
+  constructor() {
+    if (!Tooltip.#onlyInstance) {
+      Tooltip.#onlyInstance = this;
+    }
+    return Tooltip.#onlyInstance;
+  }
+
+  /** @param {this} instance */
+  getEvents(instance) {
+    const { element, render } = instance;
+
+    return {
+      pointerover: {
+        event: "pointerover",
+        handler: ({ target }) => {
+          const element = target.closest("[data-tooltip]");
+          element && render(element.getAttribute("data-tooltip"));
+        },
+      },
+
+      pointermove: {
+        event: "pointermove",
+        handler: ({ pageX, pageY }) => {
+          if (element) {
+            element.style.left = `${pageX + 8}px`;
+            element.style.top = `${pageY + 12}px`;
+          }
+        },
+      },
+
+      pointerout: {
+        event: "pointerout",
+        handler: () => element.remove(),
+      },
+    };
+  }
+
+  /** @param {'addEventListener' | 'removeEventListener'} method */
+  handleListeneres(method = "addEventListener") {
+    const events = this.getEvents(this);
+    Object.keys(events).forEach((event) => {
+      document[method](event, events[event].handler);
+    });
+  }
+
+  initialize() {
+    this.element = this.createElement();
+    this.handleListeneres();
+  }
+
+  createElement = () => {
+    const element = document.createElement("div");
+    element.style.position = "absolute";
+    element.className = `tooltip`;
+    return element;
+  };
+
+  render = (content) => {
+    this.element.textContent = content;
+    document.body.append(this.element);
+  };
+
+  remove() {
+    this.element = null;
+    this.handleListeneres("removeEventListener");
+  }
+
+  destroy() {
+    this.remove();
   }
 }
 
