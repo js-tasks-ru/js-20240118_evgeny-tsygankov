3874e8a4a476bd20203c21f7a2e9529bb88453e5
Evgeny <evtsyga@gmail.com>
2024-02-17 17:46:48 +0300

    feat: add implement 06 module - table v2

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,69 @@
-export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+import SortableTable from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
 
+const switchOrderByMap = {
+  asc: "desc",
+  desc: "asc",
+};
+
+export default class ExtendedSortableTable extends SortableTable {
+  #isSortLocally;
+
+  constructor(
+    headersConfig,
+    { data = [], sorted = {} } = {},
+    isSortLocally = true
+  ) {
+    super(headersConfig, data);
+
+    this.#isSortLocally = isSortLocally;
+
+    super.sort(sorted?.id, sorted?.order);
+    super.render();
+    this.addListeners();
+  }
+
+  addListeners() {
+    this.subElements.header.addEventListener(
+      "pointerdown",
+      this.handleCellClick
+    );
+  }
+
+  removeListeners() {
+    this.subElements.header.removeEventListener(
+      "pointerdown",
+      this.handleCellClick
+    );
+  }
+
+  sortOnServer = () => {};
+
+  sortOnClient = (clickedElement) => {
+    const orderBy = clickedElement.getAttribute("data-order");
+
+    orderBy &&
+      super.sort(
+        clickedElement.getAttribute("data-id"),
+        switchOrderByMap[orderBy]
+      );
+  };
+
+  handleCellClick = (event) => {
+    const element = event.target.closest(".sortable-table__cell");
+
+    if (element.getAttribute("data-sortable") === "true") {
+      this.sort(element);
+    }
+  };
+
+  sort(clickedElement) {
+    return this.#isSortLocally
+      ? this.sortOnClient(clickedElement)
+      : this.sortOnServer();
+  }
+
+  destroy() {
+    super.destroy();
+    this.removeListeners();
   }
 }
